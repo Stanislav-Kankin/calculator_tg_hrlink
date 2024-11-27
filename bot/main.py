a79c1b63fb62088bbdb78c5e0fb5790dcebe3c35
Stnislav-Kankin <stanislav.kankin@yandex.ru>
2024-11-27 10:29:19 +0300

    more logs

--- bot/main.py
+++ bot/main.py
@@ -483,6 +483,9 @@ def calculate_total_operations_costs(data, documents_per_year, cost_per_minute):
     print(f'printing: {time_of_printing}')
     print(f'signing: {time_of_signing}')
     print(f'archiving: {time_of_archiving}')
+    print(f'coast per min{cost_per_minute}')
+    print(f'docs per year {documents_per_year}')
+    print(f'num of hr {data['hr_specialist_count']}')
     print(total_operations_costs)
     return total_operations_costs
 
