2410e3a9035f9cf3cdddb70d434b8fb553c212f6
Stnislav-Kankin <stanislav.kankin@yandex.ru>
2024-11-27 16:21:09 +0300

    add tegs

--- bot/main.py
+++ bot/main.py
@@ -332,7 +332,7 @@ async def save_data(message: Message, state: FSMContext):
             total_paper_costs + total_logistics_costs + total_operations_costs
             )}</b> руб.\n"
         "\n"
-        f"<b>Сумма КЭДО от HRlink: {format_number(total_license_costs)}</b> руб. \n"
+        f"<u><b>Сумма КЭДО от HRlink: {format_number(total_license_costs)}</b></u> руб. \n"
         "Сюда входят: <b>Базовая лицензия</b> (рабочее пространство), <b>Лицензия</b> "
         "<b>Кадрового специалиста(ов)</b>, <b>Лицензии для сотрудников</b> (С возможностью "
         "выпустить <b>УНЭП</b>, так же в лицензию входит <b>5 СМС</b>)\n"
