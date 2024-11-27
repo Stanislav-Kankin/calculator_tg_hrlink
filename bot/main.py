c2d3fb7b5e7a4af86fced1cfc58e301013b08c69
Stnislav-Kankin <stanislav.kankin@yandex.ru>
2024-11-27 10:02:12 +0300

    странные дела

--- bot/main.py
+++ bot/main.py
@@ -472,7 +472,7 @@ def calculate_total_operations_costs(data, documents_per_year, cost_per_minute):
     # Стоимость каждой операции в минутах
     time_of_printing = typical_operations.time_of_printing
     time_of_signing = typical_operations.time_of_signing
-    time_of_archiving = typical_operations.time_of_archiving
+    time_of_archiving = typical_operations.tome_of_archiving
 
     # Общая стоимость всех операций за год
     total_operations_costs = (
