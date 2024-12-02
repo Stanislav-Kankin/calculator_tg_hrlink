b936187a0f6ba704a89869e4580e76dcf92bf218
Stnislav-Kankin <stanislav.kankin@yandex.ru>
2024-12-02 17:51:03 +0300

    debug

--- bot/main.py
+++ bot/main.py
@@ -468,6 +468,7 @@ def calculate_documents_per_year(data):
     result = employee_count * (
         documents_per_employee * (
             1 + turnover_percentage / 100))
+    logging.debug(f"Calculated documents per year: {result}")
     return result
 
 
@@ -475,6 +476,7 @@ def calculate_pages_per_year(data):
     documents_per_year = calculate_documents_per_year(data)
     pages_per_document = data['pages_per_document']
     result = documents_per_year * pages_per_document
+    logging.debug(f"Calculated pages per year: {result}")
     return result
 
 
@@ -485,6 +487,7 @@ def calculate_total_paper_costs(pages_per_year):
     result = pages_per_year * (
         paper_costs.page_cost + paper_costs.printing_cost +
         paper_costs.storage_cost + paper_costs.rent_cost)
+    logging.debug(f"Calculated total paper costs: {result}")
     return result
 
 
@@ -493,12 +496,14 @@ def calculate_total_logistics_costs(data, documents_per_year):
     hr_delivery_percentage = data.get('hr_delivery_percentage', 0)
     total_logistics_costs = courier_delivery_cost * (
         hr_delivery_percentage / 100 * documents_per_year)
+    logging.debug(f"Calculated total logistics costs: {total_logistics_costs}")
     return total_logistics_costs
 
 
 def calculate_cost_per_minute(data):
     average_salary = data['average_salary']
     working_minutes_per_month = data.get('working_minutes_per_month', 10080)
+    logging.debug(f"Calculated cost per minute: {average_salary / working_minutes_per_month}")
     return average_salary / working_minutes_per_month
 
 
@@ -513,12 +518,18 @@ def calculate_total_operations_costs(data, documents_per_year, cost_per_minute):
         (time_of_printing * cost_per_minute) +
         (time_of_archiving * cost_per_minute) +
         (time_of_signing * cost_per_minute)) * documents_per_year
+    logging.debug(f"Calculated total operations costs: {total_operations_costs}")
     return total_operations_costs
 
 
 def calculate_total_license_costs(data, license_costs):
     hr_specialist_count = data['hr_specialist_count']
     employee_count = data['employee_count']
+    logging.debug(f"Calculated total license costs: {
+        (
+            license_costs.hr_license_cost * hr_specialist_count +
+            license_costs.employee_license_cost * employee_count +
+            license_costs.main_license_cost)}")
     return (
         license_costs.hr_license_cost * hr_specialist_count +
         license_costs.employee_license_cost * employee_count +
