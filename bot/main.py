25b75de56a861952ad7f5068cc9aba10d3f7c91e
Stnislav-Kankin <stanislav.kankin@yandex.ru>
2024-12-02 17:44:10 +0300

    еуые

--- bot/main.py
+++ bot/main.py
@@ -468,7 +468,6 @@ def calculate_documents_per_year(data):
     result = employee_count * (
         documents_per_employee * (
             1 + turnover_percentage / 100))
-    logging.debug(f"Calculated documents per year: {result}")
     return result
 
 
@@ -476,7 +475,6 @@ def calculate_pages_per_year(data):
     documents_per_year = calculate_documents_per_year(data)
     pages_per_document = data['pages_per_document']
     result = documents_per_year * pages_per_document
-    logging.debug(f"Calculated pages per year: {result}")
     return result
 
 
@@ -486,31 +484,21 @@ def calculate_total_paper_costs(pages_per_year):
     session.close()
     result = pages_per_year * (
         paper_costs.page_cost + paper_costs.printing_cost +
-        paper_costs.storage_cost + paper_costs.rent_cost
-    )
-    logging.debug(f"Calculated total paper costs: {result}")
+        paper_costs.storage_cost + paper_costs.rent_cost)
     return result
 
 
 def calculate_total_logistics_costs(data, documents_per_year):
     courier_delivery_cost = data['courier_delivery_cost']
-    hr_delivery_percentage = data.get('hr_delivery_percentage')
-
-    logging.debug(f"courier_delivery_cost: {courier_delivery_cost}")
-    logging.debug(f"hr_delivery_percentage: {hr_delivery_percentage}")
-
+    hr_delivery_percentage = data.get('hr_delivery_percentage', 0)
     total_logistics_costs = courier_delivery_cost * (
-        hr_delivery_percentage / 100 * documents_per_year
-    )
-    logging.debug(f"LOGISTIC COST: {total_logistics_costs}")
-
+        hr_delivery_percentage / 100 * documents_per_year)
     return total_logistics_costs
 
 
 def calculate_cost_per_minute(data):
     average_salary = data['average_salary']
     working_minutes_per_month = data.get('working_minutes_per_month', 10080)
-    logging.debug(f"cost per min: {average_salary / working_minutes_per_month}")
     return average_salary / working_minutes_per_month
 
 
@@ -518,25 +506,13 @@ def calculate_total_operations_costs(data, documents_per_year, cost_per_minute):
     session = Session()
     typical_operations = session.query(TypicalOperations).first()
     session.close()
-
-    if not typical_operations:
-        raise ValueError("Нет данных.")
-
     time_of_printing = typical_operations.time_of_printing
     time_of_signing = typical_operations.time_of_signing
-    time_of_archiving = typical_operations.tome_of_archiving  # Исправлена опечатка
-
-    logging.debug(f"time_of_printing: {time_of_printing}")
-    logging.debug(f"time_of_signing: {time_of_signing}")
-    logging.debug(f"time_of_archiving: {time_of_archiving}")
-
+    time_of_archiving = typical_operations.tome_of_archiving
     total_operations_costs = (
         (time_of_printing * cost_per_minute) +
         (time_of_archiving * cost_per_minute) +
-        (time_of_signing * cost_per_minute)
-    ) * documents_per_year
-    logging.debug(f"TOTAL OPER COST: {total_operations_costs}")
-
+        (time_of_signing * cost_per_minute)) * documents_per_year
     return total_operations_costs
 
 
@@ -546,8 +522,7 @@ def calculate_total_license_costs(data, license_costs):
     return (
         license_costs.hr_license_cost * hr_specialist_count +
         license_costs.employee_license_cost * employee_count +
-        license_costs.main_license_cost
-    )
+        license_costs.main_license_cost)
 
 
 def calculate_costs(data, paper_costs, license_costs, typical_operations):
