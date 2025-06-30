7b6aff1ac72f772a4cf01a7749689b1ec88f4357
Stanislav.Kankin <stanislav.kankin@gmail.com>
2025-06-30 15:16:54 +0300

    new broadcast

--- bot/broadcast.py
+++ bot/broadcast.py
@@ -11,14 +11,6 @@ async def send_broadcast(bot: Bot):
     base_dir = os.path.dirname(os.path.abspath(__file__))
     images_dir = os.path.join(base_dir, 'images')
 
-    # Формируем список путей к изображениям
-    # image_paths = [
-    #     os.path.join(images_dir, '1.jpg'),
-    #     os.path.join(images_dir, '2.jpg'),
-    #     os.path.join(images_dir, '3.jpg'),
-    #     os.path.join(images_dir, '4.jpg')
-    # ]
-
     image_paths = [
         os.path.join(images_dir, 'march12.jpg')
     ]
@@ -32,27 +24,38 @@ async def send_broadcast(bot: Bot):
     users = session.query(UserData).all()
 
     message_text = (
-        "Уважаемые пользователи!\n\n"
-        "Недавно в нашем боте произошел инцидент: злоумышленник получил доступ и рассылал сообщения от его имени.\n\n"
-        "<b>Что важно знать:</b>\n"
-        "✔️ Утечки личных данных не было – бот не хранит и не обрабатывает конфиденциальную информацию.\n"
-        "✔️ Уязвимость устранена – мы оперативно обновили систему и теперь бот полностью безопасен.\n"
-        "⚠️ <b>Не переходите по подозрительным ссылкам</b> из прошлых сообщений – они могут быть вредоносными.\n\n"
-        "Мы приносим извинения за доставленные неудобства и благодарим за понимание. "
-        "Бот снова работает в штатном режиме – вы можете пользоваться им как прежде.\n\n"
-        "Если у вас есть вопросы – готовы ответить."
+        "Здравствуйте!\n\n"
+        "Вчера наш бот подвергся атаке. Злоумышленники получили доступ к отправке сообщений и рассылали материалы неподобающего характера. "
+        "Нам искренне жаль, что вам пришлось столкнуться с неудобствами.\n\n"
+        "Киберпреступления сегодня — глобальная проблема. Пожалуйста, будьте внимательны и не переходите по подозрительным ссылкам.\n\n"
+        "Сейчас все под контролем. Мы усилили меры безопасности и восстановили нормальную работу. "
+        "А также сделали <a href='https://t.me/hrl_calcbot'>нового бота</a> для вашего удобства.\n\n"
+        "Благодарим за понимание."
     )
 
     # Формируем медиагруппу
-    media = [InputMediaPhoto(
-        media=FSInputFile(image_path)) for image_path in image_paths]
-    media[0].caption = message_text
-    media[0].parse_mode = 'HTML'
+    # media = [InputMediaPhoto(
+    #     media=FSInputFile(image_path)) for image_path in image_paths]
+    # media[0].caption = message_text
+    # media[0].parse_mode = 'HTML'
 
     # Отправляем сообщения каждому пользователю
+    # for user in users:
+    #     try:
+    #         await bot.send_media_group(chat_id=user.user_id, media=media)
+    #     except Exception as e:
+    #         print(f"Ошибка при отправке сообщения пользователю {user.user_id}: {e}")
+
+    # session.close()
+
+    # Отправляем текстовые сообщения каждому пользователю
     for user in users:
         try:
-            await bot.send_media_group(chat_id=user.user_id, media=media)
+            await bot.send_message(
+                chat_id=user.user_id,
+                text=message_text,
+                parse_mode='HTML'
+            )
         except Exception as e:
             print(f"Ошибка при отправке сообщения пользователю {user.user_id}: {e}")
 
